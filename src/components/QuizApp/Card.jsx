e02438e0bfc8d972d514c6376594f24bd2b86f46
jSohnData <jihos@me.com>
2023-07-07 00:06:37 -0400

    WIP new styles

--- src/components/QuizApp/Card.jsx
+++ src/components/QuizApp/Card.jsx
@@ -4,9 +4,12 @@ export default function Card({ data, quizId, quizLength, handleAnswer }) {
 
   return(
     <div className="quiz-card">
-      <h2>Guess The Movie <span>({quizId + 1}/{quizLength})</span></h2>
+      <h2>Guess The Movie</h2>
       
-      <img src={imageUri + data[quizId].image} alt="" />
+      <figure>
+        <img src={imageUri + data[quizId].image} alt="" />
+        <p>{quizId + 1}/{quizLength}</p>
+      </figure>
 
       <ul>
         {
