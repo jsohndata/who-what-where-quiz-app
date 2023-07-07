a75318834b510a1d43d126d9d667764796e42f64
jSohnData <jihos@me.com>
2023-07-07 01:29:23 -0400

    ADDED particles

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,10 +1,25 @@
+import { useCallback } from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
 import AppLayout from "../layout/AppLayout";
 import QuizApp from "../components/QuizApp";
+
+import ParticlesOptions from '../data/bubble.json';
 import movieData from "../data/movie.json";
+
+
 export default function Quiz() {
+  const particlesInit = useCallback(
+    main => {loadFull(main)}, []
+  );
 
   return(
     <AppLayout>
+       <Particles
+          options={ParticlesOptions}
+          init={particlesInit} />
+
       <QuizApp data={movieData} />
     </AppLayout>
   );
