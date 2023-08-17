db543a0d64bac89f078340538b5ef99ad57c60b3
jSohnData <jihos@me.com>
2023-08-17 01:32:18 -0400

    ADDED mozart's ghost

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -9,6 +9,11 @@ export default function Footer () {
         <br />
         <a href={gitHubUrl} target="_blank" rel="noreferrer">See Code in Github</a>
       </p>
+
+      <p id="mozarts-ghost"><a href="https://notiempo.lol/mozartsghost/15"
+        rel="noreferrer"
+        alt="Click on it and press control-shift."
+        title="Click on it and press control-shift.">π</a></p>
     </footer>
   );
 };
